0f8f61a125052088d090274f2e5f0615ce2f0330
nhs21202 <90045608+nhs21202@users.noreply.github.com>
2023-04-06 16:39:58 +0700

    Update 2
    Mobile nav, footer

--- homepage.js
+++ homepage.js
@@ -1,3 +1,25 @@
+
+//Scroll-to-top button
+let mybutton = document.getElementById("scroll")
+
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+  // When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
+}
+
 // changing the active class on the navbar
 var item = document.querySelectorAll(".nav-item");
 item.forEach((element) => {
@@ -6,7 +28,6 @@ item.forEach((element) => {
     this.classList.add("active1");
   });
 });
-
 function openBox(event, boxName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -21,6 +42,8 @@ function openBox(event, boxName) {
   event.currentTarget.className += " active2";
 }
 
+
+//Sticky navbar
 const headerE1 = document.querySelector(".navbar");
 window.addEventListener ("scroll", () => {
   if (window.scrollY >50){
@@ -31,3 +54,12 @@ window.addEventListener ("scroll", () => {
   }
 });
 
+//Mobile navbar
+function openMobileNav(){
+  document.querySelector(".mobile-nav-section").style.display="flex";
+  document.querySelector(".navbar").style.display="none";
+}
+function closeMobileNav(){
+  document.querySelector(".mobile-nav-section").style.display="none";
+  document.querySelector(".navbar").style.display="flex";
+}
